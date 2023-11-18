09a72f25db6e4a09d98ad2a111a0120d173a3f10
hidacow <68262945+hidacow@users.noreply.github.com>
2023-11-18 19:01:35 +0800

    fix(ui): filter college with empty name

--- src/mixins/common/getCourseClassList.js
+++ src/mixins/common/getCourseClassList.js
@@ -2,7 +2,7 @@ import { getCollegesList, getMajorsList, getLessonPropertiesList } from "shu-cou
 
 export const GetCourseClassListMixin = {
   methods: {
-    getCollegesList: (p) => getCollegesList(p),
+    getCollegesList: (p) => getCollegesList(p).filter((v) => v.name !== ""),
     getMajorsList: (v, p) => getMajorsList(v, p),
     getLessonPropertiesList: () => getLessonPropertiesList(),
   }
